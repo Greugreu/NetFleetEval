6d5a097e43581e02a1cbb7ecdcb9c29d87b42850
Greugreu <greugreu@greugreu.com>
2022-01-07 09:55:57 +0100

    movies fixtures

--- src/Fixtures/MoviesFixtures.php
+++ src/Fixtures/MoviesFixtures.php
@@ -1,6 +1,24 @@
 <?php
 
+use App\Entity\Movie;
+use Doctrine\Persistence\ObjectManager;
+
 class MoviesFixtures
 {
+    public function load(ObjectManager $manager)
+    {
+        $faker = Faker\Factory::create();
 
+        for ($i = 0; $i < 10; $i++)
+        {
+            $movie = new Movie();
+            $movie->setName($faker->title);
+            $movie->setSynopsis($faker->text);
+            $movie->setGenre($faker->word);
+            $movie->setCreatedAt(new DateTimeImmutable());
+
+            $manager->persist($movie);
+        }
+        $manager->flush();
+    }
 }
